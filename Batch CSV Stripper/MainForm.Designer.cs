4db209b7d58d085da484b34b72af10465bf2d4fe
hexvenn <hexvenn@gmail.com>
2018-04-23 01:50:00 +0100

    Column stripping/row filtering implementation

--- Batch CSV Stripper/MainForm.Designer.cs
+++ Batch CSV Stripper/MainForm.Designer.cs
@@ -35,6 +35,10 @@
             this.buttonInputDirectory = new System.Windows.Forms.Button();
             this.buttonOutputDirectory = new System.Windows.Forms.Button();
             this.buttonStrip = new System.Windows.Forms.Button();
+            this.checkBoxMerge = new System.Windows.Forms.CheckBox();
+            this.checkBoxAppend = new System.Windows.Forms.CheckBox();
+            this.textBoxAppend = new System.Windows.Forms.TextBox();
+            this.checkBoxHeader = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,37 +49,37 @@
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Location = new System.Drawing.Point(12, 66);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowHeadersVisible = false;
-            this.dataGridView.Size = new System.Drawing.Size(604, 71);
+            this.dataGridView.RowHeadersWidth = 80;
+            this.dataGridView.Size = new System.Drawing.Size(760, 100);
             this.dataGridView.TabIndex = 0;
             // 
             // textBoxInputDirectory
             // 
             this.textBoxInputDirectory.Location = new System.Drawing.Point(12, 13);
             this.textBoxInputDirectory.Name = "textBoxInputDirectory";
-            this.textBoxInputDirectory.Size = new System.Drawing.Size(570, 20);
+            this.textBoxInputDirectory.Size = new System.Drawing.Size(726, 20);
             this.textBoxInputDirectory.TabIndex = 1;
             // 
             // textBoxOutputDirectory
             // 
             this.textBoxOutputDirectory.Location = new System.Drawing.Point(12, 40);
             this.textBoxOutputDirectory.Name = "textBoxOutputDirectory";
-            this.textBoxOutputDirectory.Size = new System.Drawing.Size(570, 20);
+            this.textBoxOutputDirectory.Size = new System.Drawing.Size(726, 20);
             this.textBoxOutputDirectory.TabIndex = 2;
             // 
             // listBoxLog
             // 
             this.listBoxLog.FormattingEnabled = true;
-            this.listBoxLog.Location = new System.Drawing.Point(12, 202);
+            this.listBoxLog.Location = new System.Drawing.Point(12, 233);
             this.listBoxLog.Name = "listBoxLog";
-            this.listBoxLog.Size = new System.Drawing.Size(604, 134);
+            this.listBoxLog.Size = new System.Drawing.Size(760, 121);
             this.listBoxLog.TabIndex = 3;
             // 
             // buttonInputDirectory
             // 
-            this.buttonInputDirectory.Location = new System.Drawing.Point(588, 12);
+            this.buttonInputDirectory.Location = new System.Drawing.Point(744, 13);
             this.buttonInputDirectory.Name = "buttonInputDirectory";
-            this.buttonInputDirectory.Size = new System.Drawing.Size(28, 20);
+            this.buttonInputDirectory.Size = new System.Drawing.Size(28, 21);
             this.buttonInputDirectory.TabIndex = 4;
             this.buttonInputDirectory.Text = "...";
             this.buttonInputDirectory.UseVisualStyleBackColor = true;
@@ -83,7 +87,7 @@
             // 
             // buttonOutputDirectory
             // 
-            this.buttonOutputDirectory.Location = new System.Drawing.Point(588, 39);
+            this.buttonOutputDirectory.Location = new System.Drawing.Point(744, 40);
             this.buttonOutputDirectory.Name = "buttonOutputDirectory";
             this.buttonOutputDirectory.Size = new System.Drawing.Size(28, 20);
             this.buttonOutputDirectory.TabIndex = 5;
@@ -93,19 +97,66 @@
             // 
             // buttonStrip
             // 
-            this.buttonStrip.Location = new System.Drawing.Point(12, 143);
+            this.buttonStrip.Enabled = false;
+            this.buttonStrip.Location = new System.Drawing.Point(12, 195);
             this.buttonStrip.Name = "buttonStrip";
-            this.buttonStrip.Size = new System.Drawing.Size(604, 48);
+            this.buttonStrip.Size = new System.Drawing.Size(760, 32);
             this.buttonStrip.TabIndex = 6;
             this.buttonStrip.Text = "Strip All";
             this.buttonStrip.UseVisualStyleBackColor = true;
             this.buttonStrip.Click += new System.EventHandler(this.buttonStrip_Click);
             // 
+            // checkBoxMerge
+            // 
+            this.checkBoxMerge.AutoSize = true;
+            this.checkBoxMerge.Enabled = false;
+            this.checkBoxMerge.Location = new System.Drawing.Point(12, 172);
+            this.checkBoxMerge.Name = "checkBoxMerge";
+            this.checkBoxMerge.Size = new System.Drawing.Size(89, 17);
+            this.checkBoxMerge.TabIndex = 7;
+            this.checkBoxMerge.Text = "Merge output";
+            this.checkBoxMerge.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAppend
+            // 
+            this.checkBoxAppend.AutoSize = true;
+            this.checkBoxAppend.Checked = true;
+            this.checkBoxAppend.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAppend.Location = new System.Drawing.Point(535, 172);
+            this.checkBoxAppend.Name = "checkBoxAppend";
+            this.checkBoxAppend.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxAppend.TabIndex = 8;
+            this.checkBoxAppend.Text = "Append to filename(s):";
+            this.checkBoxAppend.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAppend
+            // 
+            this.textBoxAppend.Location = new System.Drawing.Point(672, 170);
+            this.textBoxAppend.Name = "textBoxAppend";
+            this.textBoxAppend.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAppend.TabIndex = 9;
+            this.textBoxAppend.Text = "_stripped";
+            // 
+            // checkBoxHeader
+            // 
+            this.checkBoxHeader.AutoSize = true;
+            this.checkBoxHeader.Enabled = false;
+            this.checkBoxHeader.Location = new System.Drawing.Point(107, 172);
+            this.checkBoxHeader.Name = "checkBoxHeader";
+            this.checkBoxHeader.Size = new System.Drawing.Size(135, 17);
+            this.checkBoxHeader.TabIndex = 10;
+            this.checkBoxHeader.Text = "Copy header only once";
+            this.checkBoxHeader.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 348);
+            this.ClientSize = new System.Drawing.Size(784, 366);
+            this.Controls.Add(this.checkBoxHeader);
+            this.Controls.Add(this.textBoxAppend);
+            this.Controls.Add(this.checkBoxAppend);
+            this.Controls.Add(this.checkBoxMerge);
             this.Controls.Add(this.buttonStrip);
             this.Controls.Add(this.buttonOutputDirectory);
             this.Controls.Add(this.buttonInputDirectory);
@@ -131,6 +182,10 @@
         private System.Windows.Forms.Button buttonInputDirectory;
         private System.Windows.Forms.Button buttonOutputDirectory;
         private System.Windows.Forms.Button buttonStrip;
+        private System.Windows.Forms.CheckBox checkBoxMerge;
+        private System.Windows.Forms.CheckBox checkBoxAppend;
+        private System.Windows.Forms.TextBox textBoxAppend;
+        private System.Windows.Forms.CheckBox checkBoxHeader;
     }
 }
 
--- Batch CSV Stripper/MainForm.cs
+++ Batch CSV Stripper/MainForm.cs
@@ -16,6 +16,8 @@ namespace Batch_CSV_Stripper
 {
     public partial class MainForm : Form
     {
+        string[] csvFiles;
+
         public MainForm()
         {
             InitializeComponent();
@@ -32,16 +34,18 @@ namespace Batch_CSV_Stripper
             if (inputDirectoryDialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 textBoxInputDirectory.Text = inputDirectoryDialog.FileName;
-                var csvFiles = GetCSVFiles(inputDirectoryDialog.FileName);
+                csvFiles = GetCSVFiles(inputDirectoryDialog.FileName);
 
                 Log($"Found {csvFiles.Length} files in {inputDirectoryDialog.FileName}");
 
                 if (csvFiles.Length > 0)
                 {
                     var headers = GetCSVHeaders(csvFiles[0]);
-                    Log($"File headers: {string.Join(" | ", headers)}");
+                    Log($"File structure: {string.Join(" | ", headers)}");
 
                     PrepareDataGrid(headers);
+
+                    buttonStrip.Enabled = true;
                 }
             }
         }
@@ -59,7 +63,103 @@ namespace Batch_CSV_Stripper
 
         private void buttonStrip_Click(object sender, EventArgs e)
         {
+            var filters = GetRow(0);
+            var removes = GetRow(1).Select(o => o == "x").ToArray();
 
+            string outputPath = textBoxOutputDirectory.Text;
+
+            int fileNo = 0;
+
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.DoWork += new DoWorkEventHandler(
+                delegate (object o, DoWorkEventArgs args)
+                {
+                    BackgroundWorker b = o as BackgroundWorker;
+
+                    for (int i = 0; i < csvFiles.Length; i++)
+                    {
+                        b.ReportProgress(-1);
+
+                        string newName = Path.GetFileNameWithoutExtension(csvFiles[i]);
+
+                        if (checkBoxAppend.Checked)
+                        {
+                            newName += textBoxAppend.Text;
+                        }
+
+                        using (TextFieldParser parser = new TextFieldParser(csvFiles[i]))
+                        {
+                            using (StreamWriter writer = File.AppendText($@"{outputPath}\{newName}.csv"))
+                            {
+                                parser.TextFieldType = FieldType.Delimited;
+                                parser.SetDelimiters(",");
+
+                                var n = 0;
+                                while (!parser.EndOfData)
+                                {
+                                    string[] fields = parser.ReadFields();
+
+                                    List<String> strippedLine = new List<string>();
+                                    bool remove = false;
+
+                                    for (int j = 0; j < fields.Length; j++)
+                                    {
+                                        if (removes[j])
+                                        {
+                                            continue;
+                                        }
+
+                                        if (filters[j] == "")
+                                        {
+                                            strippedLine.Add(EncaseIfComma(fields[j]));
+                                            continue;
+                                        }
+
+                                        if (filters[j] == "*")
+                                        {
+                                            if (fields[j] == "")
+                                                remove = true;
+                                            continue;
+                                        }
+
+                                        if (fields[j] != filters[j])
+                                        {
+                                            remove = true;
+                                        }
+                                    }
+
+                                    if(!remove)
+                                    writer.WriteLine(string.Join(",", strippedLine));
+
+                                    n++;
+
+                                    // Only update the progress every 1000 rows, otherwise the UI would be updated too often
+                                    if(n%1000==0)
+                                    b.ReportProgress(n);
+                                }
+
+                                b.ReportProgress(n);
+                            }
+                        }
+                    }
+
+                });
+
+            bw.ProgressChanged += new ProgressChangedEventHandler(
+                delegate (object o, ProgressChangedEventArgs args)
+                {
+                    if(args.ProgressPercentage==-1)
+                    {
+                        fileNo++;
+                        Log($"[{fileNo}/{csvFiles.Length}] Processing file {csvFiles[fileNo-1]}...");
+                        Log($"");
+                    }
+                    else
+                    UpdateLastLogLine($"Processed {args.ProgressPercentage} lines...");
+                });
+
+            bw.RunWorkerAsync();
         }
 
         private string[] GetCSVFiles(string directoryPath)
@@ -73,18 +173,58 @@ namespace Batch_CSV_Stripper
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
-                return parser.ReadFields();  
+                return parser.ReadFields();
             }
         }
 
         private void PrepareDataGrid(string[] headers)
         {
+            dataGridView.Columns.Clear();
+            dataGridView.Rows.Clear();
 
+            foreach (var name in headers)
+            {
+                dataGridView.Columns.Add(name, name);
+            }
+
+            dataGridView.Rows.Add();
+            dataGridView.Rows[0].HeaderCell.Value = "Filter";
+            dataGridView.Rows.Add();
+            dataGridView.Rows[1].HeaderCell.Value = "Remove";
+        }
+
+        private string EncaseIfComma(string text)
+        {
+            if (text.Contains(","))
+            {
+                return "\"" + text + "\"";
+            }
+
+            return text;
+        }
+
+        private string[] GetRow(int row)
+        {
+            string[] cells = new string[dataGridView.Rows[row].Cells.Count];
+
+            for (int i = 0; i < dataGridView.Rows[row].Cells.Count; i++)
+            {
+                cells[i] = dataGridView[i, row].Value == null ? "" : dataGridView[i, row].Value.ToString();
+            }
+
+            return cells;
         }
 
         private void Log(string text)
         {
-            listBoxLog.Items.Add($"[{DateTime.Now.ToString("HH:mm")}]: {text}");
+            listBoxLog.Items.Add($"[{DateTime.Now:HH:mm}]: {text}");
+            listBoxLog.TopIndex = listBoxLog.Items.Count - 1;
+        }
+
+        private void UpdateLastLogLine(string text)
+        {
+            listBoxLog.Items[listBoxLog.Items.Count - 1] = $"[{DateTime.Now:HH:mm}]: {text}";
+            listBoxLog.TopIndex = listBoxLog.Items.Count - 1;
         }
     }
 
